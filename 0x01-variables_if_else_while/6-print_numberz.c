23f44663ee290ea0b8deb63d1954506f88cda184
Gig1414 <gig88ali@gmail.com>
2023-09-11 19:18:47 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,19 @@
-#include <stdlib.h> 
-#include <time.h>
 #include <stdio.h>
-
 /**
- * main - prints number from 0 to 9.
- * 
- * Return: Always 0 (success)
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
 */
-  int main(void)
+  
+int main(void)
 {
-  int i;
+  int c;
 
-   for (i = 48;  i < 58;  i++)
- {
-     putchar(i);
-  }
+   c = 0;
+  while
+     (c < 10) {
+     putchar(c + '0');
+              c++;
+       }
   putchar('\n');
   return (0);
-  }
+}
